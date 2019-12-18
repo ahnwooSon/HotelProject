8141459d27bbf3ca8372fab17d5473479f79af79
ahnwooSon <58459114+ahnwooSon@users.noreply.github.com>
2019-12-18 19:38:09 +0900

    Add files via upload

--- java/admin/controller/AdminMainController.java
+++ java/admin/controller/AdminMainController.java
@@ -1,25 +1,35 @@
 package admin.controller;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import member.model.Member;
 
 @Controller
 public class AdminMainController {
 
 	private final String command = "/main.ad";
 	private final String getPage = "adMain";
-	
+	private final String gotoPage = "redirect:/main.jsp";
 
-	
 	@RequestMapping(value = command, method = RequestMethod.GET)
-	public String doAction(HttpServletResponse response) {
-		
-		
+	public String doAction(HttpServletResponse response, HttpSession session) {
+
+		if ((Member) session.getAttribute("loginfo") == null) {
+			return gotoPage;
+		}
+		Member loginfo = (Member) session.getAttribute("loginfo");
+		String adCheck = loginfo.getM_email();
+		if (!adCheck.equals("admin@admin.com")) {
+			return gotoPage;
+		}
+
 		return getPage;
-		
+
 	}
 
 }
